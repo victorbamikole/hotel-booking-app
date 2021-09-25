a832cfa6b921473f95aa9a778cad342435ab6894
apcexchange <princepee10@gmail.com>
2021-09-25 19:45:16 +0100

    Splash screen modified

--- app/src/main/java/com/example/hbapplicationgroupb/ui/onboarding_screen/BoardingFragment01.kt
+++ app/src/main/java/com/example/hbapplicationgroupb/ui/onboarding_screen/BoardingFragment01.kt
@@ -1,10 +1,11 @@
-package com.example.hbapplicationgroupb
+package com.example.hbapplicationgroupb.ui.onboarding_screen
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.hbapplicationgroupb.R
 
 
 class BoardingFragment01 : Fragment() {
--- app/src/main/java/com/example/hbapplicationgroupb/ui/splashscreen/SplashScreenFragment.kt
+++ app/src/main/java/com/example/hbapplicationgroupb/ui/splashscreen/SplashScreenFragment.kt
@@ -1,6 +1,7 @@
-package com.example.hbapplicationgroupb
+package com.example.hbapplicationgroupb.ui.splashscreen
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -8,9 +9,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
+import com.example.hbapplicationgroupb.R
 import com.example.hbapplicationgroupb.databinding.FragmentSplashScreenBinding
-import com.google.android.material.animation.AnimationUtils
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenFragment : Fragment(R.layout.fragment_splash_screen) {
@@ -18,31 +21,48 @@ class SplashScreenFragment : Fragment(R.layout.fragment_splash_screen) {
     private var _binding : FragmentSplashScreenBinding? = null
     private val binding get() = _binding!!
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
+        requireActivity().window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         _binding = FragmentSplashScreenBinding.inflate(layoutInflater,container,false)
+
+        setUpSplashScreen()
+        // Inflate the layout for this fragment
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val hotelTextViewAnimation = android.view.animation.AnimationUtils.loadAnimation(requireContext(),R.anim.slide_from_top_animation)
-        val voyageTextViewAnimation = android.view.animation.AnimationUtils.loadAnimation(requireContext(),R.anim.slide_from_bottom_animation)
+    private fun setUpSplashScreen(){
+        val hotelTextViewAnimation = android.view.animation.AnimationUtils.loadAnimation(requireContext(),
+            R.anim.slide_from_top_animation
+        )
+        val voyageTextViewAnimation = android.view.animation.AnimationUtils.loadAnimation(requireContext(),
+            R.anim.slide_from_bottom_animation
+        )
 
         binding.hotelTv.startAnimation(hotelTextViewAnimation)
         binding.voyageTv.startAnimation(voyageTextViewAnimation)
-        
-        val splashScreenTimeout = 5000
-        Handler(Looper.getMainLooper()).postDelayed({
-            findNavController().navigate(R.id.action_splashScreenFragment_to_onboarding_01Fragment)
-            activity?.finish()
-        },splashScreenTimeout.toLong())
 
+        val splashScreenTimeout = 3500
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(R.id.action_splashScreenFragment_to_onboardingFragment01
+                ,null,
+                NavOptions.Builder()
+                    .setPopUpTo(R.id.splashScreenFragment,true).build())
+        },splashScreenTimeout.toLong())
     }
 
+    override fun onDetach() {
+        requireActivity().window.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        super.onDetach()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding=null
+    }
 
 }
