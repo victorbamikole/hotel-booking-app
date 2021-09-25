3d11eb380d808815cbdc4b269d67f8ebeaf2baab
Johnnycake01 <oyesinajohnson@gmail.com>
2021-09-25 04:33:58 -0700

    added comment to activity and fragment

--- app/src/main/java/com/example/hbapplicationgroupb/MainActivity.kt
+++ app/src/main/java/com/example/hbapplicationgroupb/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.hbapplicationgroupb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hbapplicationgroupb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //var of type of binding class created for xml file
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)//set binding to root layout
     }
 }
--- app/src/main/java/com/example/hbapplicationgroupb/ui/review/ReviewPageFragment.kt
+++ app/src/main/java/com/example/hbapplicationgroupb/ui/review/ReviewPageFragment.kt
@@ -9,9 +9,11 @@ import com.example.hbapplicationgroupb.databinding.FragmentReviewPageBinding
 
 
 class ReviewPageFragment : Fragment(R.layout.fragment_review_page) {
+    //var of type of binding class created for xml file
     private lateinit var binding: FragmentReviewPageBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //set binding to bind views when views have created
         binding = FragmentReviewPageBinding.bind(view)
 
     }
