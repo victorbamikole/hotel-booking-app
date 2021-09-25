b98f2c9f7e972028d2946054843738d0be99828b
Johnnycake01 <oyesinajohnson@gmail.com>
2021-09-25 13:39:15 -0700

    successfully created the recycler view and recyclerView Adapter

--- app/src/main/java/com/example/hbapplicationgroupb/ui/review/ReviewPageFragmentRVAdapter.kt
+++ app/src/main/java/com/example/hbapplicationgroupb/ui/review/ReviewPageFragmentRVAdapter.kt
@@ -1,5 +1,51 @@
 package com.example.hbapplicationgroupb.ui.review
 
-class ReviewPageFragmentRVAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hbapplicationgroupb.R
+import com.example.hbapplicationgroupb.model.UserReview
+
+class ReviewPageFragmentRVAdapter:
+    RecyclerView.Adapter<ReviewPageFragmentRVAdapter.ReviewPageFragmentViewHolder>() {
+    //list of items recycler view
+    private val listOfReview:MutableList<UserReview> = mutableListOf()
+
+    //internal class for holding recycler layout views together
+    inner class ReviewPageFragmentViewHolder(view: View):RecyclerView.ViewHolder(view){
+        val profileImage:ImageView = view
+            .findViewById(R.id.fragment_review_page_recycler_imageView_personProfile)
+        val profileName:TextView = view
+            .findViewById(R.id.fragment_review_page_recycler_tv_personName)
+        val review:TextView = view
+            .findViewById(R.id.fragment_review_page_recycler_tv_userReview)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ReviewPageFragmentViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.fragment_review_page_recycler_view_layout,parent,false)
+        return ReviewPageFragmentViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ReviewPageFragmentViewHolder, position: Int) {
+        val listOfReviewPosition = listOfReview[position]
+        holder.profileImage.setBackgroundResource(listOfReviewPosition.image)
+        holder.profileName.text = listOfReviewPosition.name
+        holder.review.text = listOfReviewPosition.review
+    }
+
+    override fun getItemCount(): Int = listOfReview.size
+
+    fun getListOfReviews(listOf:List<UserReview>){
+        listOfReview.clear()
+        listOfReview.addAll(listOf)
+        notifyDataSetChanged()
+    }
 
 }
