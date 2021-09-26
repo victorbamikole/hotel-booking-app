a331243a02d2a3dd33baf883d3eb1754fbb592a4
Johnnycake01 <oyesinajohnson@gmail.com>
2021-09-26 15:55:08 -0700

    added new logic to star rating view to show toast and update progressbar

--- app/src/main/java/com/example/hbapplicationgroupb/ui/review/AddReviewPageFragment.kt
+++ app/src/main/java/com/example/hbapplicationgroupb/ui/review/AddReviewPageFragment.kt
@@ -6,12 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
+import android.widget.RatingBar
+import android.widget.Toast
 import com.example.hbapplicationgroupb.R
 import com.example.hbapplicationgroupb.databinding.FragmentAddReviewPageBinding
 import com.example.hbapplicationgroupb.databinding.FragmentReviewPageBinding
 
 class AddReviewPageFragment : Fragment(R.layout.fragment_add_review_page) {
-
+    var userRatings = 0
     //var of type of binding class created for xml file
     private lateinit var binding: FragmentAddReviewPageBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -20,6 +22,23 @@ class AddReviewPageFragment : Fragment(R.layout.fragment_add_review_page) {
         binding = FragmentAddReviewPageBinding.bind(view)
         //to remove extra colour on top of toolbar
         requireActivity().window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+
+        //read rating when user click on star and display text
+        binding.fragmentAddReviewPageShowRating.onRatingBarChangeListener =
+            RatingBar.OnRatingBarChangeListener { p0, p1, p2 ->
+                userRatings = p0.rating.toInt()
+                Toast.makeText(requireContext(),"star $p1 clicked and has ratings of $userRatings",
+                    Toast.LENGTH_SHORT).show()
+                when(p1.toInt()){
+                    1 -> "Very poor".also { binding.fragmentAddReviewPageRatingRemark.text = it }
+                    2 -> "Poor".also { binding.fragmentAddReviewPageRatingRemark.text = it }
+                    3 -> "Good".also { binding.fragmentAddReviewPageRatingRemark.text = it }
+                    4 -> "Very Good".also { binding.fragmentAddReviewPageRatingRemark.text = it }
+                    5 -> "Excellent".also { binding.fragmentAddReviewPageRatingRemark.text = it }
+                }
+
+
+            }
     }
     override fun onDetach() {
         //clear flag on detach of fragment
