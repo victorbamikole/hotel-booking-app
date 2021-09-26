5b03f3e5306e2bd75a90c3c41dea974771fc432d
Johnnycake01 <oyesinajohnson@gmail.com>
2021-09-25 19:29:37 -0700

    completed add review page design

--- app/src/main/java/com/example/hbapplicationgroupb/ui/review/AddReviewPageFragment.kt
+++ app/src/main/java/com/example/hbapplicationgroupb/ui/review/AddReviewPageFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import com.example.hbapplicationgroupb.R
 import com.example.hbapplicationgroupb.databinding.FragmentAddReviewPageBinding
 import com.example.hbapplicationgroupb.databinding.FragmentReviewPageBinding
@@ -17,6 +18,12 @@ class AddReviewPageFragment : Fragment(R.layout.fragment_add_review_page) {
         super.onViewCreated(view, savedInstanceState)
         //set binding to bind views when views have created
         binding = FragmentAddReviewPageBinding.bind(view)
-
+        //to remove extra colour on top of toolbar
+        requireActivity().window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+    }
+    override fun onDetach() {
+        //clear flag on detach of fragment
+        requireActivity().window.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        super.onDetach()
     }
 }
