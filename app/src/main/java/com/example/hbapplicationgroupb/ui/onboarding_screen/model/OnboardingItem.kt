688fe2424b40ece1f52dc507af0922d08ac3d8a9
apcexchange <princepee10@gmail.com>
2021-09-27 15:52:19 +0100

    created model for viewpager

--- app/src/main/java/com/example/hbapplicationgroupb/ui/onboarding_screen/model/OnboardingItem.kt
+++ app/src/main/java/com/example/hbapplicationgroupb/ui/onboarding_screen/model/OnboardingItem.kt
@@ -1,4 +1,7 @@
 package com.example.hbapplicationgroupb.ui.onboarding_screen.model
 
-class OnboardingItem {
-}
+class OnboardingItem(
+    val onboardingImage: Int,
+    val title: String,
+    val description: String
+)
