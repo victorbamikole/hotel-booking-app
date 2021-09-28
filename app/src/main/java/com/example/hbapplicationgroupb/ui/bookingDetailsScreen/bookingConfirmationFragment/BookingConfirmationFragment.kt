12f30e6e88044c9f1ed6f40abafc76765b8fe425
Samuelungbede1 <samuel.onum1@gmail.com>
2021-09-28 15:33:32 +0100

    Added Functionality to pop all Bottom Sheets in the Booking Details Screen

--- app/src/main/java/com/example/hbapplicationgroupb/ui/bookingDetailsScreen/bookingConfirmationFragment/BookingConfirmationFragment.kt
+++ app/src/main/java/com/example/hbapplicationgroupb/ui/bookingDetailsScreen/bookingConfirmationFragment/BookingConfirmationFragment.kt
@@ -1,10 +1,8 @@
-package com.example.hbapplicationgroupb.ui.bookingConfirmationFragment
+package com.example.hbapplicationgroupb.ui.bookingDetailsScreen.bookingConfirmationFragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.example.hbapplicationgroupb.R
 import com.example.hbapplicationgroupb.databinding.FragmentBookingConfirmationBinding
 
