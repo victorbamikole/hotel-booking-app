ecfb93b50a0d896c73fd070038ae09e430723b67
anthonyidoko <idokoanthony86@gmail.com>
2021-09-29 15:53:41 +0100

    wrote UI tests for the various user auth screens

---
+++ app/src/androidTest/java/com/example/hbapplicationgroupb/ui/userAuthenticationScreen/RegistrationFragmentTest.kt
@@ -0,0 +1,57 @@
+package com.example.hbapplicationgroupb.ui.userAuthenticationScreen
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.hbapplicationgroupb.MainActivity
+import com.example.hbapplicationgroupb.R
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class RegistrationFragmentTest{
+
+/**    @get:Rule
+    val activityRule: ActivityScenarioRule<MainActivity> =
+        ActivityScenarioRule(MainActivity::class.java)
+
+    @Test//Test Name field in Registration Screen
+    fun test_isRegistrationScreenUserNameFieldVisible(){
+        onView(withId(R.id.tv_reg_user_name))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test//Test Email field in Registration Screen
+    fun test_isRegistrationScreenUserEmailFieldVisible(){
+        onView(withId(R.id.tv_reg_user_email))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+    }
+
+    @Test//Test Password field in Registration Screen
+    fun test_isRegistrationScreenUserPasswordFieldVisible(){
+        onView(withId(R.id.tv_reg_user_password))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+    }
+
+    @Test//Test Radio button in Registration Screen
+    fun test_isRegistrationScreenRadioButtonCheckable(){
+        onView(withId(R.id.btn_radio))
+            .check(ViewAssertions.matches(ViewMatchers.isChecked()))
+    }
+
+    @Test//Test Register button field in Registration Screen
+    fun test_isRegisterButtonClickable(){
+        onView(withId(R.id.btn_register))
+            .check(ViewAssertions.matches(ViewMatchers.isClickable()))
+    }*/
+
+}
