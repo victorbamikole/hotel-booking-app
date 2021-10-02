8891757deb0eca18c7800f41394227402655045b
victorbamikole <victorbamikole92@gmail.com>
2021-10-02 16:07:20 +0100

    Top Deals Added

---
+++ app/src/main/java/com/example/hbapplicationgroupb/adapter/TopDealsAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.hbapplicationgroupb.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hbapplicationgroupb.R
+import com.example.hbapplicationgroupb.databinding.TopDealsRecyclerviewBinding
+import com.example.hbapplicationgroupb.databinding.TopHotelsRecyclerviewBinding
+import com.example.hbapplicationgroupb.model.TopHotels
+
+class TopDealsAdapter(var topDeals: List<TopHotels>):
+    RecyclerView.Adapter<TopDealsAdapter.DealsViewHolder>(){
+    class DealsViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        private val binding: TopDealsRecyclerviewBinding =
+            TopDealsRecyclerviewBinding.bind(itemView)
+        val topDealImage = binding.topDealImage
+        val topDealName = binding.topDealName
+        val topDealPrice = binding.topDealPrice
+        val topDealRating = binding.topDealRating
+        val topDealPercent = binding.topDealPercent
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DealsViewHolder {
+        val view =
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.top_deals_recyclerview, parent, false)
+        return TopDealsAdapter.DealsViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: DealsViewHolder, position: Int) {
+        holder.itemView.apply {
+            val currentItem = topDeals[position]
+            holder.topDealImage.setImageResource(currentItem.hotelImage)
+            holder.topDealName.text = currentItem.name
+            holder.topDealPrice.text = currentItem.price
+            holder.topDealRating.text = currentItem.rating
+            holder.topDealPercent.text = currentItem.percent
+            setOnClickListener {
+                findNavController().navigate(R.id.hotelDescriptionFragment)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return topDeals.size
+    }
+}
--- app/src/main/java/com/example/hbapplicationgroupb/ui/topdeals/TopDealsFragment.kt
+++ app/src/main/java/com/example/hbapplicationgroupb/ui/topdeals/TopDealsFragment.kt
@@ -5,56 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.hbapplicationgroupb.R
+import com.example.hbapplicationgroupb.adapter.TopDealsAdapter
+import com.example.hbapplicationgroupb.adapter.TopHotelsAdapter
+import com.example.hbapplicationgroupb.databinding.FragmentTopDealsBinding
+import com.example.hbapplicationgroupb.databinding.FragmentTopHotelsBinding
+import com.example.hbapplicationgroupb.model.TopHotels
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class TopDealsFragment : Fragment(R.layout.fragment_top_deals) {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [TopDealsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class TopDealsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    var binding: FragmentTopDealsBinding? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentTopDealsBinding.bind(view)
+
+        val topDeals =
+            listOf(
+                TopHotels(R.drawable.hotel2, "Sheraton Grand", "$599", "4 star hotel", "80%"),
+                TopHotels(R.drawable.hotel3, "GitHub Grand", "$999", "5 star hotel", "90%"),
+                TopHotels(R.drawable.hotel2, "Radison Blue", "$1299", "4 star hotel", "80%"),
+                TopHotels(R.drawable.hotel3, "Jetro Hols", "$899", "5 star hotel", "100%")
+            )
+
+        //Inflate the top hotels recycler view layout to the fragment class
+        val myAdapter = TopDealsAdapter(topDeals)
+        binding!!.topDealsRecyclerView.adapter = myAdapter
+        binding!!.topDealsRecyclerView.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_top_deals, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TopDealsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TopDealsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
