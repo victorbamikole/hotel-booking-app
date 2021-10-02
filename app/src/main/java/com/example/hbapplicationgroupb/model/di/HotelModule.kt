86b192b5a80f30ba4078108c46683e73479d11a8
seunawonugba <seun.awonugba@decagon.dev>
2021-10-03 00:54:54 +0100

    removed deprecated hilt dependencies

---
+++ app/src/main/java/com/example/hbapplicationgroupb/model/di/HotelModule.kt
@@ -0,0 +1,10 @@
+package com.example.hbapplicationgroupb.model.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+
+@Module
+@InstallIn(ActivityComponent::class)
+object HotelModule {
+}
