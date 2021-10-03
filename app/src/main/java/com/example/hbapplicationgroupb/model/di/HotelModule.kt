a24a97e801913a91e889944e4473fc8e67add6cf
seunawonugba <seun.awonugba@decagon.dev>
2021-10-03 01:12:38 +0100

    added hilt module as a singleton componet

--- app/src/main/java/com/example/hbapplicationgroupb/model/di/HotelModule.kt
+++ app/src/main/java/com/example/hbapplicationgroupb/model/di/HotelModule.kt
@@ -3,8 +3,9 @@ package com.example.hbapplicationgroupb.model.di
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.components.SingletonComponent
 
 @Module
-@InstallIn(ActivityComponent::class)
+@InstallIn(SingletonComponent::class)
 object HotelModule {
 }
