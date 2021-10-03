57786211dbbae1d5cbf9c8b247330fbd06f8d245
seunawonugba <seun.awonugba@decagon.dev>
2021-10-03 23:20:55 +0100

    Implement hotel app module

---
+++ app/src/main/java/com/example/hbapplicationgroupb/model/api/RetrofitServiceInterface.kt
@@ -0,0 +1,4 @@
+package com.example.hbapplicationgroupb.model.api
+
+interface RetrofitServiceInterface {
+}
--- app/src/main/java/com/example/hbapplicationgroupb/model/di/HotelModule.kt
+++ app/src/main/java/com/example/hbapplicationgroupb/model/di/HotelModule.kt
@@ -1,11 +1,35 @@
 package com.example.hbapplicationgroupb.model.di
 
+import com.example.hbapplicationgroupb.model.api.RetrofitServiceInterface
+import com.example.hbapplicationgroupb.model.constants.Constants.BASE_URL
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object HotelModule {
+
+    @Singleton
+    @Provides
+    fun provideDataInHotelModule(): RetrofitServiceInterface {
+
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level= HttpLoggingInterceptor.Level.BODY
+        val client: OkHttpClient = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+            .create(RetrofitServiceInterface::class.java)
+
+    }
 }
