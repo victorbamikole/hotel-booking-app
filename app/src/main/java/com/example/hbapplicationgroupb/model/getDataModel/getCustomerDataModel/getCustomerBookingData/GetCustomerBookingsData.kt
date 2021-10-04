de83e5d441d071d15dda5762a605f6243e78e5f6
Iniyealakeret2 <iniyealakeret1@gmail.com>
2021-10-04 12:11:01 +0100

    Entities of the data classes added

--- app/src/main/java/com/example/hbapplicationgroupb/model/getDataModel/getCustomerDataModel/getCustomerBookingData/GetCustomerBookingsData.kt
+++ app/src/main/java/com/example/hbapplicationgroupb/model/getDataModel/getCustomerDataModel/getCustomerBookingData/GetCustomerBookingsData.kt
@@ -1,8 +1,12 @@
 package com.example.hbapplicationgroupb.model.getDataModel.getCustomerDataModel.getCustomerBookingData
 
-import com.example.hbapplicationgroupb.model.getDataModel.getCustomerDataModel.getCustomerBookingData.GetCustomerBookingDataItems
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity(tableName = "Customer Booking History")
 data class GetCustomerBookingsData(
+    @PrimaryKey
+    val id: Int,
     val statusCode: String,
     val success: Boolean,
     val data: List<GetCustomerBookingDataItems>,
