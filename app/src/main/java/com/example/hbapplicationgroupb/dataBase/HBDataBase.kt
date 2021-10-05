0c561b20f0b37d1d2aeaeb79a90fb487f8113efc
Samuelungbede1 <samuel.onum1@gmail.com>
2021-10-05 13:05:44 +0100

    Bug at the  bookNow Button Fixed

--- app/src/main/java/com/example/hbapplicationgroupb/dataBase/HBDataBase.kt
+++ app/src/main/java/com/example/hbapplicationgroupb/dataBase/HBDataBase.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.hbapplicationgroupb.model.Hotels
+import com.example.hbapplicationgroupb.model.hotelTopDeals.HotelTopDealItems
 
 
-@Database(/**entities = [Post::class, Comment::class], version = 1, exportSchema = false*/)
+@Database(entities = [HotelTopDealItems::class], version = 1, exportSchema = false)
 abstract class HBDataBase : RoomDatabase() {
 
 //    abstract fun postDao(): PostDao
