548f7be5339891d242e491ffcbbacd1ee0dd5dbf
Johnnycake01 <oyesinajohnson@gmail.com>
2021-10-09 15:02:33 -0700

    nav-graph added to manifest and add deeplink to reset password fragment

--- app/src/main/java/com/example/hbapplicationgroupb/ui/userAuthenticationScreen/ResetPasswordFragment.kt
+++ app/src/main/java/com/example/hbapplicationgroupb/ui/userAuthenticationScreen/ResetPasswordFragment.kt
@@ -5,16 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.example.hbapplicationgroupb.R
 import com.example.hbapplicationgroupb.databinding.FragmentResetPasswordBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ResetPasswordFragment : Fragment(R.layout.fragment_reset_password) {
+
     private lateinit var binding:FragmentResetPasswordBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentResetPasswordBinding.bind(view)
+
+        val userToken = arguments?.getString("token")
+        val userEmail = arguments?.getString("email")
+
     }
 
 }
