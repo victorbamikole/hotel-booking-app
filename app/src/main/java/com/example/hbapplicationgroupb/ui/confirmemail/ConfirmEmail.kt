0eca1087e524df432d3c2cd281918966e54bc9ca
Johnnycake01 <oyesinajohnson@gmail.com>
2021-10-12 15:20:12 -0700

    create email confirmation page

---
+++ app/src/main/java/com/example/hbapplicationgroupb/ui/confirmemail/ConfirmEmail.kt
@@ -0,0 +1,12 @@
+package com.example.hbapplicationgroupb.ui.confirmemail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.hbapplicationgroupb.R
+
+class ConfirmEmail : Fragment(R.layout.fragment_confirm_email) {
+
+}
