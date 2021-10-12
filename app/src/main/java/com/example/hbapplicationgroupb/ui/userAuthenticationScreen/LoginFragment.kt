e7d40a6fb3eac80445b475bea915c909aa0bf627
seunawonugba <seun.awonugba@decagon.dev>
2021-10-12 05:23:07 +0100

    disabled login button when email and password is empty

--- app/src/main/java/com/example/hbapplicationgroupb/ui/userAuthenticationScreen/LoginFragment.kt
+++ app/src/main/java/com/example/hbapplicationgroupb/ui/userAuthenticationScreen/LoginFragment.kt
@@ -1,6 +1,8 @@
 package com.example.hbapplicationgroupb.ui.userAuthenticationScreen
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -26,6 +28,26 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         binding.btnLogin.setOnClickListener {
            findNavController().navigate(R.id.action_loginFragment_to_exploreFragment2)
         }
+
+        //Toggle enable sign up button
+        binding.tvUserLoginEmail.addTextChangedListener(loginButtonHandler)
+        binding.tvUserPassword.addTextChangedListener(loginButtonHandler)
+
+    }
+
+
+    //Login button handler
+    //If the two text fields are empty, the login button will be disabled
+    private val loginButtonHandler : TextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            val userLoginEmail : String = binding.tvUserLoginEmail.text.toString().trim()
+            val userLoginPassword : String = binding.tvUserPassword.text.toString().trim()
+            binding.btnLogin.isEnabled = userLoginEmail.isNotEmpty() && userLoginPassword.isNotEmpty()
+        }
+
+        override fun afterTextChanged(p0: Editable?) {}
 
     }
 }
