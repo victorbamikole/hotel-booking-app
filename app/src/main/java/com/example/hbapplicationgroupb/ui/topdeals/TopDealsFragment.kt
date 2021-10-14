a9d1fefbbc049d76f0b0e28295fc8b7b41d0ed49
Samuelungbede1 <samuel.onum1@gmail.com>
2021-10-14 15:38:06 +0100

    created new layout for top deal hotel recyclerView and fetched top deal hotel  data from api

--- app/src/main/java/com/example/hbapplicationgroupb/ui/topdeals/TopDealsFragment.kt
+++ app/src/main/java/com/example/hbapplicationgroupb/ui/topdeals/TopDealsFragment.kt
@@ -1,42 +1,55 @@
 package com.example.hbapplicationgroupb.ui.topdeals
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.hbapplicationgroupb.R
 import com.example.hbapplicationgroupb.databinding.FragmentTopDealsBinding
 import com.example.hbapplicationgroupb.model.TopHotels
+import com.example.hbapplicationgroupb.ui.bottom_nav_screens.ExploreHomeAdapter
+import com.example.hbapplicationgroupb.ui.bottom_nav_screens.ExploreHomeAdapter2
+import com.example.hbapplicationgroupb.viewModel.RoomViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class TopDealsFragment : Fragment(R.layout.fragment_top_deals) {
-
     var binding: FragmentTopDealsBinding? = null
+    private val roomViewModel : RoomViewModel by viewModels()
+    private val myAdapter = TopDealsAdapter()
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentTopDealsBinding.bind(view)
 
-        //List of top deals items
-        val topDeals =
-            listOf(
-                TopHotels(R.drawable.hotel2, "Sheraton Grand", "$599", "4 star hotel", "80%"),
-                TopHotels(R.drawable.hotel3, "GitHub Grand", "$999", "5 star hotel", "90%"),
-                TopHotels(R.drawable.hotel2, "Radison Blue", "$1299", "4 star hotel", "80%"),
-                TopHotels(R.drawable.hotel3, "Jetro Hols", "$899", "5 star hotel", "100%")
-            )
+        initViewModel()
+        //Fetch Top Deals From APi
+        roomViewModel.getTopDeals(20, 1)
 
-        //Inflate the top hotels recycler view layout to the fragment class
-        val myAdapter = TopDealsAdapter(topDeals)
-        binding!!.topDealsRecyclerView.adapter = myAdapter
+        //Inflate the top deals recycler view layout to the fragment class
         binding!!.topDealsRecyclerView.layoutManager =
             LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+
         binding?.topDealsFragmentBackArrow?.setOnClickListener {
             findNavController()
                 .navigate(R.id.action_topDealsFragment_to_exploreFragment2)
         }
-
     }
 
+    //This function observes the TopDeals LiveData and populates the RecyclerView UI
+    private fun initViewModel() {
+        roomViewModel.allTopDeals.observe(viewLifecycleOwner,{
+            if (it != null){
+                myAdapter.populateTopDeals(it.data)
+                Log.d("Homefrag", "${it.data}")
 
+                binding!!.topDealsRecyclerView.adapter = myAdapter
+                myAdapter.notifyDataSetChanged()
+            }
+        })
+    }
 }
