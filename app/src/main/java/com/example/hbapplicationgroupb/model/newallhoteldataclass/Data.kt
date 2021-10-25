568f75915723d35aefbe93469aecfa033d487011
Johnnycake01 <oyesinajohnson@gmail.com>
2021-10-25 15:03:03 -0700

    delete unUsed data class

--- app/src/main/java/com/example/hbapplicationgroupb/model/newallhoteldataclass/Data.kt
+++
@@ -1,9 +0,0 @@
-package com.example.hbapplicationgroupb.model.newallhoteldataclass
-
-data class Data(
-    val currentPage: Int,
-    val numberOfPages: Int,
-    val pageItems: List<PageItem>,
-    val pageSize: Int,
-    val previousPage: Int
-)
--- app/src/main/java/com/example/hbapplicationgroupb/model/newallhoteldataclass/NewAllHotelDataClass.kt
+++
@@ -1,8 +0,0 @@
-package com.example.hbapplicationgroupb.model.newallhoteldataclass
-
-data class NewAllHotelDataClass(
-    val `data`: Data,
-    val message: String,
-    val statusCode: Int,
-    val succeeded: Boolean
-)
