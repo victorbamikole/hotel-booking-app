2d974dd7827149748161e8ffdc8f7d049e1b1323
victorbamikole <victorbamikole92@gmail.com>
2021-10-26 16:22:06 +0100

    UI Bugs fixed

--- app/src/main/java/com/example/hbapplicationgroupb/ui/privacy_policy_screen/PrivacyPolicyFragment.kt
+++ app/src/main/java/com/example/hbapplicationgroupb/ui/privacy_policy_screen/PrivacyPolicyFragment.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.hbapplicationgroupb.R
 import com.example.hbapplicationgroupb.databinding.FragmentPrivacyPolicyBinding
 
@@ -15,6 +16,11 @@ class PrivacyPolicyFragment : Fragment(R.layout.fragment_privacy_policy) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentPrivacyPolicyBinding.bind(view)
+
+        binding.fragmentPrivacyPolicyBackArrowViev.setOnClickListener {
+            findNavController()
+                .navigate(R.id.action_privacyPolicyFragment_to_profileFragment2)
+        }
 
     }
 
